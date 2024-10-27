878f7ea088a574c062f57255d99a093d89ee4f69
Yomna Mohsen <ymn_mohsen@yahoo.com>
2024-10-27 13:52:05 +0300

    version 1

--- js/app.js
+++ js/app.js
@@ -22,7 +22,8 @@
  * Define Global Variables
  * 
 */
-
+const sections = document.querySelectorAll('section');
+const navlist = document.getElementById('navbar__list');
 
 /**
  * End Global Variables
@@ -38,13 +39,56 @@
  * 
 */
 
-// build the nav
+// function to build the nav-menu dynamically and add scroll
 
+function build_nav(){
+
+// foreach loop to iterate on all sections
+sections.forEach((section) => {
+    let li_item = document.createElement('li');
+    let anchor = document.createElement('a');
+    li_item.textContent = section.dataset.nav;
+    li_item.classList.add("menu__link");
+    navlist.appendChild(li_item);
+    // Scroll to section on link click
+    li_item.addEventListener('click', function(){
+        section.scrollIntoView({behavior :"smooth", block: "end"});
+    });
+});
+}
+
+//Function to check if element in viewport
+
+function inviewport(elem){
+    const rect= elem.getBoundingClientRect()
+     return (
+                rect.top >= -300 && 
+                rect.left >= 0 && 
+                rect.bottom <= (window.innerHeight + 300|| document.documentElement.clientHeight + 300) && 
+                rect.right <= (window.innerWidth || document.documentElement.clientWidth) 
+            );
+
+}
 
 // Add class 'active' to section when near top of viewport
+function makeActive(){
+    const items_arr = document.getElementsByTagName('li');
+    for (let i=0; i < sections.length; i++) {
+        //check position of element
+         if (inviewport(sections[i])) {
+        //apply active state on current section and corresponding Nav link
+        sections[i].classList.add("your-active-class");
+        items_arr[i].classList.add("active");
+        console.log(i + 1);
+        } else {
+        //Remove active state from other section and corresponding Nav link
+        sections[i].classList.remove("your-active-class");
+        items_arr[i].classList.remove("active");
+        }
+    }
+}
 
 
-// Scroll to anchor ID using scrollTO event
 
 
 /**
@@ -54,9 +98,13 @@
 */
 
 // Build menu 
+build_nav();
 
-// Scroll to section on link click
+// Set sections as active on scroll
+window.addEventListener('scroll', makeActive);
+ 
 
-// Set sections as active
+
+
 
 
